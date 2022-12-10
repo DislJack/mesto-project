60fc6151db543c5a504b531cc94c1c25c5e64a5c
Yaroslav Shadrin <yaroslavbrok@gmail.com>
2022-12-10 22:15:13 +0300

    Составлена структура проекта по разделам Переменные, Функции и Логика объявления функции. Добавлен новый popup контейнер на добавление картинок с названиями. Настроена форма закрыть и открыть контейнер, а так же сохранить без изменений.

--- js/index.js
+++ js/index.js
@@ -1,29 +1,26 @@
-/* Разработка логики модальных окон */
-// Открытие popup контейнера по клику
-
-function popupOpened() {
-  let popupOpened = document.querySelector('.popup');
-  if (popupOpened.classList.contains('popup_opened') === true) {
-    popupOpened.classList.remove('popup_opened');
-  } else {
-    popupOpened.classList.add('popup_opened');
-  }
-}
-
-let editButton = document.querySelector('.profile__edit-button');
-editButton.addEventListener('click', popupOpened);
-
-// Закрытие popup контейнера по клику
-
-let closeButton = document.querySelector('.form__close-button');
-closeButton.addEventListener('click', popupOpened);
-
-// Изменение данных в форме при сохранении формы
+// Переменные и постоянные
 
 const formElement = document.querySelector('.form');
 const nameInput = document.querySelector('input[name=name]');
 const jobInput = document.querySelector('input[name=job]');
+let editButton = document.querySelector('.profile__edit-button');
+let addButton = document.querySelector('.profile__add-button');
+let closeButton = document.querySelectorAll('.form__close-button');
+let submitButton = document.querySelectorAll('.form__submit-button');
+let likeButtons = document.querySelectorAll('.element__like');
 
+// Функции
+// Функция открытия popup контейнеров
+function popupOpened(index) {
+  let popupOpened = document.querySelectorAll('.popup');
+  if (popupOpened[index].classList.contains('popup_opened') === true) {
+    popupOpened[index].classList.remove('popup_opened');
+  } else {
+    popupOpened[index].classList.add('popup_opened');
+  }
+}
+
+// Функция настройки ввода формы
 function formSubmitHandler(evt) {
   evt.preventDefault();
   let name = document.querySelector('.profile__heading-name');
@@ -32,18 +29,38 @@ function formSubmitHandler(evt) {
   job.textContent = jobInput.value;
 }
 
+// Логика управления элементами
+// Открыли popup контейнеры
+editButton.addEventListener('click', () => {
+  popupOpened(0);
+}, false);
+addButton.addEventListener('click', () => {
+  popupOpened(1);
+}, false);
+
+// Кнопки закрыть на popup контейнерах
+closeButton[0].addEventListener('click', () => {
+  popupOpened(0);
+}, false);
+closeButton[1].addEventListener('click', () => {
+  popupOpened(1);
+}, false)
+
+// Изменение данных в форме при сохранении формы
 formElement.addEventListener('submit', formSubmitHandler);
-let submitButton = document.querySelector('.form__submit-button');
-submitButton.addEventListener('click', popupOpened);
 
-// Функция описания смены цвета лайк элемента
+// Кнопки подтвердить отправку форму на закрытие
+submitButton[0].addEventListener('click', () => {
+  popupOpened(0);
+}, false);
+submitButton[1].addEventListener('click', () => {
+  popupOpened(1);
+}, false)
 
-let likeButtons = document.querySelectorAll('.element__like');
-let index = 0;
-
+// Меняем цвет лайк кнопки
 for (i = 0; i < likeButtons.length; i++) {
   let index = i;
-  likeButtons[i].addEventListener('click', () => { // тут проблема с i оно не передаётся
+  likeButtons[i].addEventListener('click', () => {
     if (likeButtons[index].classList.contains('element__like_active') === true) {
       likeButtons[index].classList.remove('element__like_active');
     } else {
