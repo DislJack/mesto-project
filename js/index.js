84f6f34eca18c3b0ff2ebf1a7e1382c2040a4735
Yaroslav Shadrin <yaroslavbrok@gmail.com>
2022-12-20 00:51:52 +0300

    Добавлена функция добавления карточки. Настроена смена лайк иконки. Настроена кнопка удаления карточки. Отрегулирована высота карточки так, чтобы все карточки были одинаковыми.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,46 @@
 // Переменные и постоянные
 
-const formElement = document.querySelector('.form');
+const formElement = document.querySelector('form[name=form]');
 const nameInput = document.querySelector('input[name=name]');
 const jobInput = document.querySelector('input[name=job]');
-let editButton = document.querySelector('.profile__edit-button');
-let addButton = document.querySelector('.profile__add-button');
-let closeButton = document.querySelectorAll('.form__close-button');
-let submitButton = document.querySelectorAll('.form__submit-button');
-let likeButtons = document.querySelectorAll('.element__like');
+const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
+const closeButton = document.querySelectorAll('.form__close-button');
+const submitButton = document.querySelectorAll('.form__submit-button');
+const likeButtons = document.querySelectorAll('.element__like');
+const elements = document.querySelector('.elements');
+const titleInput = document.querySelector('input[name=name-place]');
+const linkInput = document.querySelector('input[name=link]');
+const formCard = document.querySelector('form[name=formCard]');
+const deleteButton = document.querySelectorAll('.element__delete');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+  ];
+  console.log(initialCards);
 
 // Функции
 // Функция открытия popup контейнеров
@@ -29,6 +62,28 @@ function formSubmitHandler(evt) {
   job.textContent = jobInput.value;
 }
 
+// Функция добавления карточки
+function addCard() {
+  const elementTemplate = document.querySelector('#element').content;
+  const element = elementTemplate.querySelector('.element').cloneNode(true);
+  element.querySelector('.element__delete').addEventListener('click', function (evt) {
+    evt = element.closest('.element');
+    evt.remove();
+  });
+  element.querySelector('.element__like').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('element__like_active');
+  });
+  element.querySelector('.element__image').src = linkInput.value;
+  element.querySelector('.element__title').textContent = titleInput.value;
+  elements.prepend(element);
+}
+
+// Функция подтверждения формы загрузки карточки
+function formSubmitCard(evt) {
+  evt.preventDefault();
+  addCard();
+}
+
 // Логика управления элементами
 // Открыли popup контейнеры
 editButton.addEventListener('click', () => {
@@ -48,6 +103,7 @@ closeButton[1].addEventListener('click', () => {
 
 // Изменение данных в форме при сохранении формы
 formElement.addEventListener('submit', formSubmitHandler);
+formCard.addEventListener('submit', formSubmitCard);
 
 // Кнопки подтвердить отправку форму на закрытие
 submitButton[0].addEventListener('click', () => {
@@ -66,5 +122,13 @@ for (i = 0; i < likeButtons.length; i++) {
     } else {
       likeButtons[index].classList.add('element__like_active');
     }
+  });
+}
+
+for (let i = 0; i < deleteButton.length; i++) {
+  let index = i;
+  deleteButton[i].addEventListener('click', () => {
+    const element = deleteButton[index].closest('.element');
+    element.remove();
   });
 }
